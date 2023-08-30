1b53fdc2d30b5fb48e67c7f550eb6928aec37142
Anas Firly <anasfirly20@gmail.com>
2023-08-30 12:25:35 +0400

    adding fade animations

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <button type="button" @click="toggle">Toggle</button>
+  <transition name="fade">
+    <h2 v-if="flag">Hello world!</h2>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data(){
+    return {
+      flag: false
+    }
+  },
+  methods: {
+    toggle(){
+      this.flag = !this.flag
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter-from{
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+
+.fade-leave-to{
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+
+</style>
