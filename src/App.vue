0c9855c5b398e1a26de3fdec05ad0620beba3d58
Anas Firly <anasfirly20@gmail.com>
2023-08-30 12:52:09 +0400

    adding appear transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <button type="button" @click="toggle">Toggle</button>
-  <transition name="fade">
+
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag">Hello world!</h2>
+    <h2 v-else key="secondary">Another hello!</h2>
+  </transition> -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
   </transition>
+  
 </template>
 
 <script>
@@ -10,7 +17,7 @@ export default {
   name: "App",
   data(){
     return {
-      flag: false
+      flag: true
     }
   },
   methods: {
@@ -22,17 +29,55 @@ export default {
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px
+}
+
 .fade-enter-from{
   opacity: 0;
 }
 
 .fade-enter-active {
-  transition: all 0.25s linear;
+  transition: all 1s linear;
 }
 
 .fade-leave-to{
-  transition: all 0.25s linear;
+  transition: all 1s linear;
   opacity: 0;
 }
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 1s linear
+
+}
+
+@keyframes zoom-in {
+  from{
+    transform: scale(0,0);
+  }
+  to {
+    transform: scale(1,1);
+  }
+  
+}
+
+@keyframes zoom-out {
+  from{
+    transform: scale(1,1);
+  }
+  to {
+    transform: scale(0,0);
+  }
+  
+}
+
 
 </style>
